4216c876b455dc076fb0cca96941282afa76dee1
1v4nk0 <1vancherk3s@gmail.com>
2024-07-16 17:20:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Counter />
+    </>
   );
 }
 
-export default App;
+function Counter() {
+  const firstDate = new Date("december 12 2028");
+  const date = new Date("december 12 2028");
+  const [count, setCount] = useState(1);
+  const [step, setStep] = useState(1);
+  date.setDate(date.getDate() + count * step)
+  // let newDate = new Date(date.getDate() + count);
+
+
+
+
+  const countPlus = () => {
+     setCount(count + 1);
+  };
+  const countMinus = () => {
+     setCount(count - 1);
+  };
+
+  const stepPlus = () => {
+     setStep(step + 1);
+  };
+  const stepMinus = () => {
+     setStep(step - 1);
+  };
+  
+
+
+
+  return (
+    <>
+      <div className="step-container">
+        <button className="minus" onClick={stepMinus}>
+          -
+        </button>
+        <div>Step: {step}</div>
+        <button className="plus" onClick={stepPlus}>
+          +
+        </button>
+      </div>
+      <div className="count-container">
+        <button className="minus" onClick={countMinus}>
+          -
+        </button>
+        <div>Count: {count}</div>
+        <button className="plus" onClick={countPlus}>
+          +
+        </button>
+      </div>
+      <div className="display-container">
+        <div>{count === 0 ? `Today is ${firstDate.toDateString()}` : `${count*step} day(s) from ${firstDate.toDateString()} is ${date.toDateString()}`}  </div>
+      </div>
+    </>
+  );
+}
